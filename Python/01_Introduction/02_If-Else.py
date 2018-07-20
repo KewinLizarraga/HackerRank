10bfd00329d3ed28c0e11a146d43afc7e2064e2c
Kewin <kewin392@gmail.com>
2018-07-20 15:55:09 -0500

    If Else

--- Python/01_Introduction/02_If-Else.py
+++ Python/01_Introduction/02_If-Else.py
@@ -1,7 +1,9 @@
-# If n is odd print Weird
-# If n is even and in the inclusive range of 2 to 5, print Not Weird
-# If n is even and in the inclusive ranfe of 6 to 20, print Weird
-# If n is even and greater than 20, print Not Weird
+"""
+If n is odd print Weird
+If n is even and in the inclusive range of 2 to 5, print Not Weird
+If n is even and in the inclusive ranfe of 6 to 20, print Weird
+If n is even and greater than 20, print Not Weird
+"""
 
 N = int(input())
 
