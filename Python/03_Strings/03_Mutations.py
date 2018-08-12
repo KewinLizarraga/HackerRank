6bc4465ad48bf22e182fb958857a78116acf16ab
Kewin <kewin392@gmail.com>
2018-08-12 02:19:50 -0500

    Change the character at a given index

--- Python/03_Strings/03_Mutations.py
+++ Python/03_Strings/03_Mutations.py
@@ -1,3 +1,7 @@
+"""
+Read a given string, change the character at a given index and then print the modified string.
+"""
+
 def mutate_string(string, position, character):
     l = list(string)
     l[position] = character
